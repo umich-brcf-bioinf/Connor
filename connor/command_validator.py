5e01fac70e6e7396acc85491fd40778a62e9b9ee
cgates <cgates@umich.edu>
2016-08-24 17:05:40 -0400

    CON-61 (cgates): refactored command validation to consolidate dup code and reduce complexity

--- connor/command_validator.py
+++ connor/command_validator.py
@@ -1,25 +1,30 @@
 '''Validates command preconditions.
 Specifically checks that the command, arguments, and environment
 (e.g. input/output directories or files) are consistent and plausible.
-Each validation function evaluates a specific precondition.
-Each function is allowed to:
- * change the environment (e.g. create a dir)
- * change the args (replace the original output dir with a new temp output dir)
- * raise a UsageException if things look problematic
+Each validation function evaluates a specific precondition raising a
+UsageException if things look problematic or logging a warning if the condition
+can be forced.
 '''
 from __future__ import print_function, absolute_import, division
-
+import itertools
 import os
 import connor.utils as utils
 import connor.samtools as samtools
-import itertools
 
-def _check_input_bam_exists(args, log=None):
+_SAMPLE_SIZE = 1000
+
+def _log_force_or_raise(args, log, msg):
+    if args.force:
+        log.warning(msg + ' (**forcing**)')
+    else:
+        raise utils.UsageError(msg + ' Are you sure? (--force to proceed)')
+
+def _check_input_bam_exists(args, log=None): #pylint: disable=unused-argument
     if not os.path.exists(args.input_bam):
         raise utils.UsageError(("Specified input [{}] does not exist. Review "
                                 "inputs and try again.").format(args.input_bam))
 
-def _check_input_bam_valid(args, log=None):
+def _check_input_bam_valid(args, log=None): #pylint: disable=unused-argument
     try:
         bamfile = samtools.alignment_file(args.input_bam, 'rb')
         bamfile.close()
@@ -27,7 +32,7 @@ def _check_input_bam_valid(args, log=None):
         raise utils.UsageError(("Specified input [{}] not a valid BAM. Review "
                                 "inputs and try again.").format(args.input_bam))
 
-def _check_input_bam_indexed(args, log=None):
+def _check_input_bam_indexed(args, log=None): #pylint: disable=unused-argument
     bamfile = samtools.alignment_file(args.input_bam, 'rb')
     try:
         bamfile.fetch()
@@ -37,71 +42,70 @@ def _check_input_bam_indexed(args, log=None):
     finally:
         bamfile.close()
 
-def _check_input_bam_not_connor_generated(args, log=None):
+def _check_input_bam_not_deduped(args, log=None):
     bamfile = samtools.alignment_file(args.input_bam, 'rb')
     header = bamfile.header
     bamfile.close()
-    for pg_item in header.get('PG', []):
-        if pg_item.get('PN', None) == samtools.CONNOR_PG_PN:
-            if args.force:
-                msg = ('Specified input [{}] has already been processed with '
-                       'Connor (**forcing**)').format(args.input_bam)
-                log.warning(msg)
-                return
-            else:
-                msg = ('Specified input [{}] has already been processed with '
-                   'Connor. Are you sure? '
-                   '(--force to proceed)').format(args.input_bam)
-                raise utils.UsageError(msg)
+    names = set([pg_item.get('PN', None) for pg_item in header.get('PG', [])])
+    if samtools.CONNOR_PG_PN in names:
+        msg = ('Specified input [{}] has already been processed with '
+               'Connor.').format(args.input_bam)
+        _log_force_or_raise(args, log, msg)
 
-def _check_input_bam_not_empty(args, log=None):
+def _check_input_bam_not_empty(args, log=None): #pylint: disable=unused-argument
     bamfile = samtools.alignment_file(args.input_bam, 'rb')
     try:
         next(bamfile.fetch())
     except StopIteration:
-        raise utils.UsageError(("Specified input [{}] is empty").format(args.input_bam))
+        msg = "Specified input [{}] is empty"
+        raise utils.UsageError(msg.format(args.input_bam))
     finally:
         bamfile.close()
 
-def _check_input_bam_paired(args, log=None):
+def _check_input_bam_paired(args, log=None): #pylint: disable=unused-argument
     bamfile = samtools.alignment_file(args.input_bam, 'rb')
     try:
-        for alignment in bamfile.fetch():
+        for alignment in itertools.islice(bamfile.fetch(), _SAMPLE_SIZE):
             if alignment.is_paired:
                 return
-        raise utils.UsageError(("Specified input [{}] does not contain paired reads").format(args.input_bam))
     finally:
         bamfile.close()
-
+    msg = ('Specified input [{}] does not appear to contain paired '
+           'reads').format(args.input_bam)
+    _log_force_or_raise(args, log, msg)
 
 
 def _check_input_bam_barcoded(args, log=None):
-    bamfile = samtools.alignment_file(args.input_bam, 'rb')
-    softclipped_l = 0
-    softclipped_r = 0
-    threshold = 0.80
-    total_l = 0
-    total_r = 0
-    sample_size = 1000
-    for alignment in itertools.islice(bamfile.fetch(), sample_size):
-        if alignment.is_reverse:
-            total_r += 1
-            if alignment.cigartuples[-1][0] == 4:
-                softclipped_r += 1
-        else:
-            total_l += 1
-            if alignment.cigartuples[0][0] == 4:
-                softclipped_l += 1
-    left_freq = softclipped_l/total_l
-    right_freq = softclipped_r/total_r
-    if left_freq < threshold or right_freq < threshold:
-        raise utils.UsageError(("Specified input [{}] reads do not appear to have barcodes").format(args.input_bam))
+    #pylint: disable=invalid-name
+    SOFTCLIP_OP = 4
+    SOFTCLIP_THRESHOLD = 0.80
 
+    def is_edge_softclipped(align):
+        edge_index = -1 if align.is_reverse else 0
+        return int(align.cigartuples[edge_index][0] == SOFTCLIP_OP)
+
+    def strand(align):
+        return 'reverse' if align.is_reverse else 'forward'
+
+    softclipped = {'forward': [], 'reverse': []}
+    bamfile = samtools.alignment_file(args.input_bam, 'rb')
+    try:
+        for align in itertools.islice(bamfile.fetch(), _SAMPLE_SIZE):
+            softclipped[strand(align)].append(is_edge_softclipped(align))
+    finally:
+        bamfile.close()
+
+    forward_freq = sum(softclipped['forward'])/len(softclipped['forward'])
+    reverse_freq = sum(softclipped['reverse'])/len(softclipped['reverse'])
+    if min(forward_freq, reverse_freq) < SOFTCLIP_THRESHOLD:
+        msg = ('Specified input [{}] reads do not appear to have '
+               'barcodes').format(args.input_bam)
+        _log_force_or_raise(args, log, msg)
 
 _VALIDATIONS = [_check_input_bam_exists,
                 _check_input_bam_valid,
                 _check_input_bam_indexed,
-                _check_input_bam_not_connor_generated,
+                _check_input_bam_not_deduped,
                 _check_input_bam_not_empty,
                 _check_input_bam_paired]
 
--- test/command_validator_test.py
+++ test/command_validator_test.py
@@ -7,7 +7,7 @@ from __future__ import print_function, absolute_import, division
 from argparse import Namespace
 import os
 from testfixtures.tempdirectory import TempDirectory
-import connor.command_validator as command_validator
+import connor.command_validator as validator
 import connor.utils as utils
 import test.samtools_test as samtools_test
 from test.utils_test import BaseConnorTestCase
@@ -31,11 +31,11 @@ class MockTask(object):
 
 class CommandValidatorTest(BaseConnorTestCase):
     def test_Validations(self):
-        function_names = [f.__name__ for f in command_validator._VALIDATIONS]
+        function_names = [f.__name__ for f in validator._VALIDATIONS]
         self.assertEqual(['_check_input_bam_exists',
                           '_check_input_bam_valid',
                           '_check_input_bam_indexed',
-                          '_check_input_bam_not_connor_generated',
+                          '_check_input_bam_not_deduped',
                           '_check_input_bam_not_empty',
                           '_check_input_bam_paired'],
                          function_names)
@@ -43,11 +43,11 @@ class CommandValidatorTest(BaseConnorTestCase):
     def test_preflight_runsAllValidations(self):
         task1 = MockTask()
         task2 = MockTask()
-        command_validator._VALIDATIONS = [task1.execute,
-                                          task2.execute]
+        validator._VALIDATIONS = [task1.execute,
+                                  task2.execute]
         args = Namespace()
         log = self.mock_logger
-        command_validator.preflight(args, log)
+        validator.preflight(args, log)
 
         self.assertTrue(task1.execute_called)
         self.assertEqual(task1.args, args)
@@ -62,7 +62,7 @@ class CommandValidatorTest(BaseConnorTestCase):
             tmp_dir.write('input.bam', b'foo')
             input_bam_path = os.path.join(tmp_dir.path, 'input.bam')
             args = Namespace(input_bam=input_bam_path)
-            command_validator._check_input_bam_exists(args)
+            validator._check_input_bam_exists(args)
             self.ok()
 
     def test_input_bam_exists_raisesUsageError(self):
@@ -71,7 +71,7 @@ class CommandValidatorTest(BaseConnorTestCase):
             args = Namespace(input_bam=input_bam_path)
             self.assertRaisesRegexp(utils.UsageError,
                                     r'\[.*input.bam\] does not exist',
-                                    command_validator._check_input_bam_exists,
+                                    validator._check_input_bam_exists,
                                     args)
 
     def test_check_input_bam_valid(self):
@@ -85,7 +85,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=False)
             args = Namespace(input_bam=input_bam_path)
-            command_validator._check_input_bam_valid(args)
+            validator._check_input_bam_valid(args)
             self.ok()
 
     def test_check_input_bam_valid_raisesUsageError(self):
@@ -95,7 +95,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
             args = Namespace(input_bam=input_bam_path)
             self.assertRaisesRegexp(utils.UsageError,
                                     r'\[.*input.bam\] not a valid BAM',
-                                    command_validator._check_input_bam_valid,
+                                    validator._check_input_bam_valid,
                                     args)
 
     def test_check_input_bam_indexed(self):
@@ -109,7 +109,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path)
-            command_validator._check_input_bam_indexed(args)
+            validator._check_input_bam_indexed(args)
             self.ok()
 
 
@@ -126,7 +126,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
             args = Namespace(input_bam=input_bam_path)
             self.assertRaisesRegexp(utils.UsageError,
                                     r'\[.*input.bam\] is not indexed',
-                                    command_validator._check_input_bam_indexed,
+                                    validator._check_input_bam_indexed,
                                     args)
 
     def test_check_input_bam_not_connor_generated(self):
@@ -141,7 +141,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_not_connor_generated(args)
+            validator._check_input_bam_not_deduped(args)
             self.ok()
 
 
@@ -161,7 +161,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                      r'.*Are you sure.*force')
             self.assertRaisesRegexp(utils.UsageError,
                                     regex,
-                                    command_validator._check_input_bam_not_connor_generated,
+                                    validator._check_input_bam_not_deduped,
                                     args)
 
     def test_check_input_bam_not_connor_generated_noPgHeader(self):
@@ -175,7 +175,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_not_connor_generated(args)
+            validator._check_input_bam_not_deduped(args)
             self.ok()
 
     def test_check_input_bam_not_connor_generated_noPnHeader(self):
@@ -190,7 +190,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_not_connor_generated(args)
+            validator._check_input_bam_not_deduped(args)
             self.ok()
 
     def test_check_input_bam_not_connor_generated_warnIfForce(self):
@@ -205,8 +205,8 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=True)
-            command_validator._check_input_bam_not_connor_generated(args,
-                                                                    log=self.mock_logger)
+            validator._check_input_bam_not_deduped(args,
+                                                   log=self.mock_logger)
             warnings = self.mock_logger._log_calls['WARNING']
             self.assertEqual(1, len(warnings))
             regex = (r'\[.*input.bam\] has already been processed with Connor'
@@ -226,7 +226,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
             args = Namespace(input_bam=input_bam_path, force=False)
             self.assertRaisesRegexp(utils.UsageError,
                                     r'\[.*input.bam\] is empty',
-                                    command_validator._check_input_bam_not_empty,
+                                    validator._check_input_bam_not_empty,
                                     args)
 
     def test_check_input_bam_not_empty(self):
@@ -240,7 +240,7 @@ readNameA1|99|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''.replace("|", "\t")
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_not_empty(args)
+            validator._check_input_bam_not_empty(args)
             self.ok()
 
     def test_check_input_bam_paired_raisesUsageError(self):
@@ -255,10 +255,10 @@ readNameA1|{flag}|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            regex = r'\[.*input.bam\] does not contain paired reads'
+            regex = r'\[.*input.bam\] does not appear to contain paired reads'
             self.assertRaisesRegexp(utils.UsageError,
                                     regex,
-                                    command_validator._check_input_bam_paired,
+                                    validator._check_input_bam_paired,
                                     args)
 
     def test_check_input_bam_paired_ok(self):
@@ -275,8 +275,28 @@ readNameA1|{paired_flag}|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_paired(args)
+            validator._check_input_bam_paired(args)
             self.ok()
+
+    def test_check_input_bam_paired_forcingOk(self):
+        sam_contents = \
+'''@HD|VN:1.4|GO:none|SO:coordinate
+@SQ|SN:chr10|LN:135534747
+readNameA1|{flag}|chr10|100|20|5M|=|300|200|AAAAA|>>>>>'''
+        sam_contents = sam_contents.format(flag='16').replace("|", "\t")
+        with TempDirectory() as tmp_dir:
+            input_bam_path = samtools_test.create_bam(tmp_dir.path,
+                                                      "input.sam",
+                                                      sam_contents,
+                                                      index=True)
+            args = Namespace(input_bam=input_bam_path, force=True)
+            validator._check_input_bam_paired(args, self.mock_logger)
+        warnings = self.mock_logger._log_calls['WARNING']
+        self.assertEqual(1, len(warnings))
+        regex = (r'\[.*input.bam\] does not appear to contain paired '
+                 r'reads.*forcing')
+        self.assertRegexpMatches(warnings[0], regex)
+
 
     def test_check_input_bam_barcoded_raisesUsageError_ok(self):
         sam_contents = \
@@ -300,7 +320,7 @@ readNameA5|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_barcoded(args)
+            validator._check_input_bam_barcoded(args)
             self.ok()
 
     def test_check_input_bam_barcoded_ok_at_threshold(self):
@@ -325,10 +345,10 @@ readNameA5|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
                                                       sam_contents,
                                                       index=True)
             args = Namespace(input_bam=input_bam_path, force=False)
-            command_validator._check_input_bam_barcoded(args)
+            validator._check_input_bam_barcoded(args)
             self.ok()
 
-    def test_check_input_bam_barcoded_leftUnbarcoded_raisesUsageError(self):
+    def test_check_input_bam_barcoded_leftUnbarcodedRaisesUsageError(self):
         sam_contents = \
 '''@HD|VN:1.4|GO:none|SO:coordinate
 @SQ|SN:chr10|LN:135534747
@@ -353,10 +373,10 @@ readNameA5|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
             regex = r'\[.*input.bam\] reads do not appear to have barcodes'
             self.assertRaisesRegexp(utils.UsageError,
                                     regex,
-                                    command_validator._check_input_bam_barcoded,
+                                    validator._check_input_bam_barcoded,
                                     args)
 
-    def test_check_input_bam_barcoded_rightUnbarcoded_raisesUsageError(self):
+    def test_check_input_bam_barcoded_rightUnbarcodedRaisesUsageError(self):
         sam_contents = \
 '''@HD|VN:1.4|GO:none|SO:coordinate
 @SQ|SN:chr10|LN:135534747
@@ -381,7 +401,33 @@ readNameA5|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
             regex = r'\[.*input.bam\] reads do not appear to have barcodes'
             self.assertRaisesRegexp(utils.UsageError,
                                     regex,
-                                    command_validator._check_input_bam_barcoded,
+                                    validator._check_input_bam_barcoded,
                                     args)
 
-
+    def test_check_input_bam_barcoded_unbarcodedForcedOk(self):
+        sam_contents = \
+'''@HD|VN:1.4|GO:none|SO:coordinate
+@SQ|SN:chr10|LN:135534747
+readNameA1|99|chr10|100|20|8M|=|300|200|NNNAAAAA|>>>>>>>>
+readNameA1|147|chr10|100|20|8M|=|300|200|AAAAANNN|>>>>>>>>
+readNameA2|99|chr10|100|20|8M|=|300|200|NNNAAAAA|>>>>>>>>
+readNameA2|147|chr10|100|20|8M|=|300|200|AAAAANNN|>>>>>>>>
+readNameA3|99|chr10|100|20|3S5M|=|300|200|NNNAAAAA|>>>>>>>>
+readNameA3|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
+readNameA4|99|chr10|100|20|3S5M|=|300|200|NNNAAAAA|>>>>>>>>
+readNameA4|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
+readNameA5|99|chr10|100|20|3S5M|=|300|200|NNNAAAAA|>>>>>>>>
+readNameA5|147|chr10|100|20|5M3S|=|300|200|AAAAANNN|>>>>>>>>
+'''
+        sam_contents = sam_contents.replace("|", "\t")
+        with TempDirectory() as tmp_dir:
+            input_bam_path = samtools_test.create_bam(tmp_dir.path,
+                                                      "input.sam",
+                                                      sam_contents,
+                                                      index=True)
+            args = Namespace(input_bam=input_bam_path, force=True)
+            validator._check_input_bam_barcoded(args, self.mock_logger)
+        warnings = self.mock_logger._log_calls['WARNING']
+        self.assertEqual(1, len(warnings))
+        regex = r'\[.*input.bam\] reads do not appear to have barcodes.*forcing'
+        self.assertRegexpMatches(warnings[0], regex)
